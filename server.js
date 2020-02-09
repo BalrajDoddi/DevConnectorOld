3b7c4ef31b99c39d997e5a8e872d688f637d9270
EC2 Default User <ec2-user@ip-172-31-5-184.ap-south-1.compute.internal>
2020-02-09 21:57:26 +0000

    creating the routes and configuring it in server file and put simple test

--- server.js
+++ server.js
@@ -6,9 +6,13 @@ const app = express();
 // Connection to Database
 connectDB();
 
-app.get('/', (req, res) => {
-    res.send("Hello world...");
-});
+app.get('/', (req, res) => { res.send("Hello world..."); });
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 PORT = process.env.PORT || 5000;
 
