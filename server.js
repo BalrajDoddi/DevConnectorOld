c3eb8e3091021b81128b1b7da8497fcc556ae3c2
EC2 Default User <ec2-user@ip-172-31-5-184.ap-south-1.compute.internal>
2020-02-23 15:50:16 +0000

    User model is added and prepared for public-ip for console log

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const connectDB = require('./config/db');
+const publicIp = require('public-ip');
 
 const app = express();
 
 // Connection to Database
 connectDB();
+
+//Init Middleware
+app.use(express.json({ extended: false }));
+
 
 app.get('/', (req, res) => { res.send("Hello world..."); });
 
@@ -14,11 +19,15 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+//(async () => {console.log(await publicIp.v4());})();
 PORT = process.env.PORT || 5000;
 
 app.listen(PORT, (err) => {
-    if(!err)
-	console.log("Server started on port : " + PORT);
+    if(!err) {
+	(async () => {
+		console.log("Server started on : " + await publicIp.v4() + ":" + PORT);
+	})();
+    }
     else
 	console.log("Error : " + JSON.stringify(err, undefined, 2));
 });
