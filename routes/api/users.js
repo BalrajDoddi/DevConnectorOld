6719616b7a433f2f8e76c9c7954b44a0e3a95e1a
EC2 Default User <ec2-user@ip-172-31-5-184.ap-south-1.compute.internal>
2020-03-09 00:29:38 +0000

    Bug fix

--- routes/api/users.js
+++ routes/api/users.js
@@ -19,7 +19,8 @@ router.post('/', [
 	check('password', 'Please enter a password with 6 or more characters').isLength({
 		min: 6
 	})
-], (req, res) => {
+],
+	async (req, res) => {
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
 		return res.status(400).json({
